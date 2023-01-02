f02f053adced920c329301403a61850c52949abd
Anas Mahamed <anasmahamed03@gmail.com>
2023-01-02 06:28:14 +0000

    added a motivational phrase

--- greetings.py
+++ greetings.py
@@ -4,3 +4,5 @@ print("Welcome my friend")
 # show some cool greetings image
 
 # ask the user what he/she wants to drink 
+
+#well done on the progress you made you won't receive the rewards immediately but you'll receive them none the less
